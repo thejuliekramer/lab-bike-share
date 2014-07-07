32990cade366f80a2bbeebd972fc1d09b221e0ec
Walter Latimer, Jr <walter@walterlatimer.com>
2014-07-07 18:43:36 -0400

    Added validations for models

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,9 @@
 class Bike < ActiveRecord::Base
+	has_many :checkouts, dependent: :destroy
+	has_many :accessories, dependent: :destroy
+
+	validates :name, presence: true,
+	                 length: { minimum: 2 }
+
+
 end
