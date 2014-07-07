6a9c3c5108d8cd366e0cf54795849306aa727f65
Julie Kramer <thejuliekramer@gmail.com>
2014-07-07 18:45:38 -0400

    generated sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def login
+  end
+
+  def login_attempt
+  end
+
+  def logout
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/login'
+
+  get 'sessions/login_attempt'
+
+  get 'sessions/logout'
+
   resources :accessories
 
   resources :bikes
