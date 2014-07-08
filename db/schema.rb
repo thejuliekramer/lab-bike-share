39c46bdcecf899ec72ec46a7eb1d2e39931e116b
Frank Ortiz <ortiz_frank@hotmail.com>
2014-07-08 13:25:24 -0400

    Added some styling to login page

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 20140707222241) do
     t.datetime "updated_at"
   end
 
+  create_table "carts", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "checkouts", force: true do |t|
     t.integer  "user_id"
     t.integer  "bike_id"
@@ -44,103 +56,55 @@ ActiveRecord::Schema.define(version: 20140707222241) do
   add_index "checkouts", ["bike_id"], name: "index_checkouts_on_bike_id", using: :btree
   add_index "checkouts", ["user_id"], name: "index_checkouts_on_user_id", using: :btree
 
-  create_table "cohorts", force: true do |t|
+  create_table "photos", force: true do |t|
+    t.string   "url"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "photos", ["product_id"], name: "index_photos_on_product_id", using: :btree
+
+  create_table "products", force: true do |t|
     t.string   "name"
+    t.integer  "price_in_cents"
+    t.integer  "quantity"
     t.text     "description"
-    t.integer  "school_id"
+    t.integer  "category_id"
+    t.boolean  "featured"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
-  add_index "cohorts", ["school_id"], name: "index_cohorts_on_school_id", using: :btree
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
-  create_table "comments", force: true do |t|
-    t.integer  "user_id"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-  end
-
-  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
-  create_table "enrollments", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "cohort_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "role_id"
-    t.integer  "school_id"
-  end
-
-  add_index "enrollments", ["cohort_id"], name: "index_enrollments_on_cohort_id", using: :btree
-  add_index "enrollments", ["role_id"], name: "index_enrollments_on_role_id", using: :btree
-  add_index "enrollments", ["school_id"], name: "index_enrollments_on_school_id", using: :btree
-  add_index "enrollments", ["user_id"], name: "index_enrollments_on_user_id", using: :btree
-
-  create_table "invites", force: true do |t|
-    t.string   "email"
-    t.string   "token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "school_id"
-  end
-
-  add_index "invites", ["school_id"], name: "index_invites_on_school_id", using: :btree
-
-  create_table "roles", force: true do |t|
+  create_table "reviews", force: true do |t|
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "schools", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "subdomain"
-  end
-
-  add_index "schools", ["subdomain"], name: "index_schools_on_subdomain", unique: true, using: :btree
-
-  create_table "submissions", force: true do |t|
+    t.text     "comment"
+    t.integer  "rating"
+    t.integer  "product_id"
     t.integer  "user_id"
-    t.integer  "task_id"
-    t.text     "submission"
-    t.boolean  "correctness"
-    t.integer  "percieved_points"
-    t.integer  "actual_points"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "reviewed"
-  end
-
-  add_index "submissions", ["task_id"], name: "index_submissions_on_task_id", using: :btree
-  add_index "submissions", ["user_id"], name: "index_submissions_on_user_id", using: :btree
-
-  create_table "tasks", force: true do |t|
-    t.text     "description"
-    t.boolean  "visibility"
-    t.integer  "unit_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "title"
-  end
-
-  add_index "tasks", ["unit_id"], name: "index_tasks_on_unit_id", using: :btree
-
-  create_table "units", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "cohort_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "units", ["cohort_id"], name: "index_units_on_cohort_id", using: :btree
+  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
+
+  create_table "things", force: true do |t|
+    t.string   "goal"
+    t.string   "details"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
