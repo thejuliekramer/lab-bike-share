8dd8053ecb155ba628b186a12178949cf0171066
Walter Latimer, Jr <walter@walterlatimer.com>
2014-07-08 13:20:48 -0400

    Added image attachments to bikes, users, and accessories

--- app/controllers/accessories_controller.rb
+++ app/controllers/accessories_controller.rb
@@ -69,6 +69,6 @@ class AccessoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def accessory_params
-      params.require(:accessory).permit(:type, :not_missing)
+      params.require(:accessory).permit(:type, :not_missing, :image)
     end
 end
